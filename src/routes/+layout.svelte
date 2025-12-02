3f32b55a6d9e5b6752a8b4caad171e04f5e03804
FForFachriza <dimasavrian346@gmail.com>
2025-12-02 12:16:43 +0700

    feat (frontend):
    - add prettier - add eslint - add tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import './layout.css';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+{@render children()}
