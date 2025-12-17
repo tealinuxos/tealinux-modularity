130ad71bc33b7a71ed0652e62d42a653bfe9073a
FForFachriza <dimasavrian346@gmail.com>
2025-12-17 13:36:41 +0700

    feat (fe): sidebar

--- src/lib/components/TeaBar.svelte
+++ src/lib/components/TeaBar.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
-<<<<<<< HEAD
 	import { cn } from '$lib/utils';
 	import { teabarItems } from './teabar-items';
 	import { Button } from './ui/button';
 	import { page } from '$app/state';
 	import { Cog } from '@lucide/svelte';
-=======
->>>>>>> 0ff51a826d7fb8de34b5f0af3d1d1a8aaae1d7de
 </script>
 
 <Sidebar.Root>
@@ -15,7 +12,6 @@
 		<img class="size-8" src="tealinux.svg" alt="TeaLinuxOS Logo" />
 		<p class="text-[#54CD4C] font-bold text-xl">TeaLinuxOS</p>
 	</Sidebar.Header>
-<<<<<<< HEAD
 	<Sidebar.Content class="px-3.5 py-3">
 		{#each teabarItems as item (item.href)}
 			<Button
@@ -43,13 +39,5 @@
 			<Cog />
 			<p>Settings</p>
 		</Button>
-=======
-	<Sidebar.Content>
-		<Sidebar.Group />
-		<Sidebar.Group />
-	</Sidebar.Content>
-	<Sidebar.Footer>
-		<p>Settings</p>
->>>>>>> 0ff51a826d7fb8de34b5f0af3d1d1a8aaae1d7de
 	</Sidebar.Footer>
 </Sidebar.Root>
