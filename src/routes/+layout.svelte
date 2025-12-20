c24a0bb845e7eb5d40b02dac21fb55afc8650f2c
FForFachriza <dimasavrian346@gmail.com>
2025-12-20 17:58:21 +0700

    feat (fe): implement dark mode. the default color scheme follow system preferences

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import './layout.css';
 	import TeaBar from '$lib/components/TeaBar.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
+	import { ModeWatcher } from 'mode-watcher';
 
 	interface Props {
 		children: Snippet;
@@ -11,6 +12,7 @@
 	let { children }: Props = $props();
 </script>
 
+<ModeWatcher defaultMode="system" />
 <Sidebar.Provider>
 	<TeaBar />
 	<main class="flex flex-col flex-1 w-full">
