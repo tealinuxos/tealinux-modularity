4290f86fadad9d47e092c0203df1d862bab4a6be
FForFachriza <dimasavrian346@gmail.com>
2026-01-01 09:58:07 +0700

    refactor (fe:/): - change url "/" to "/home" - use group aliases "mainpage" as starting web point

---
+++ src/routes/(mainpage)/home/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { commands } from '$lib/commands';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const a = await commands.initConfigurationFile();
+
+		console.log(a);
+	});
+</script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
 	import './layout.css';
-	import TeaBar from '$lib/components/TeaBar.svelte';
-	import * as Sidebar from '$lib/components/ui/sidebar/index';
-	import { ModeWatcher } from 'mode-watcher';
-
+	import type { Snippet } from 'svelte';
 	interface Props {
 		children: Snippet;
 	}
@@ -12,13 +8,4 @@
 	let { children }: Props = $props();
 </script>
 
-<ModeWatcher defaultMode="system" />
-<Sidebar.Provider>
-	<TeaBar />
-	<main class="flex flex-col flex-1 w-full">
-		<Sidebar.Trigger />
-		<section class="p-4">
-			{@render children()}
-		</section>
-	</main>
-</Sidebar.Provider>
+{@render children?.()}
