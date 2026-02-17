099fa790a8f2424ee4ac8fc6515d5bd8cb5c7d5b
fadhil riyanto <me@fadev.org>
2026-02-17 11:00:11 +0700

    add env safety-net
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,26 @@
 fn main() {
-	tauri_build::build()
+    match std::env::var("TEALINUX_BUILD") {
+        Ok(val) => {
+            println!("cargo:warning=TEALINUX_BUILD is set to: {}", val);
+            tauri_build::build()
+        }
+        Err(_) => {
+            // If not set, set to dev
+            println!("cargo:warning=Environment variable TEALINUX_BUILD is not set during build. Automatically set to dev. See https://github.com/tealinuxos/tealinux-modularity#building");
+            unsafe 
+            {
+                std::env::set_var("TEALINUX_BUILD", "dev");
+            }
+            if let Ok(val) = std::env::var("TEALINUX_BUILD")
+            {
+                println!("cargo:warning=TEALINUX_BUILD is set to: {}", val);
+                tauri_build::build()
+            }
+            else
+            {
+                eprintln!("error: Environment variable TEALINUX_BUILD is not set during build. Aborting build. see https://github.com/tealinuxos/tealinux-modularity#building");
+                std::process::exit(1);
+            }
+        }
+    }
 }
---
+++ src-tauri/src/utils/tealinux_build_env.rs
@@ -0,0 +1,28 @@
+use std::env;
+
+#[derive(Debug, PartialEq)]
+pub enum BuildType {
+    Production,
+    Dev
+}
+
+pub fn tealinux_build_env() -> Result<BuildType, ()> {
+    match option_env!("TEALINUX_BUILD") {
+        Some(val) => {
+            if val == "production" {
+                return Ok(BuildType::Production);
+            } else if val == "prod" {
+                return Ok(BuildType::Production);
+            } else if val == "dev" {
+                return Ok(BuildType::Dev);
+            } else {
+                println!("Error TEALINUX_BUILD is not set properly, see readme please");
+            return Err(());
+            }
+        }
+        None => {
+            println!("Error TEALINUX_BUILD not found during build, see readme please");
+            return Err(());
+        },
+    }
+}
